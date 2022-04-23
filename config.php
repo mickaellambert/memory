0ebf3cfee8642c96c299740325c034372f61025b
Compte perso <mickael.lambert2@gmail.com>
2022-04-23 12:18:41 +0200

    refactor config directory + add some configs

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+# Fichier listant les constantes permettant une configuration personnalisée de notre jeu
+
+define('TIMER', 10);
+define('NB_CARDS', 12); // Doit être pair
+define('COLUMNS', 6);
+define('LINES', 4);
+define('NB_PLAYERS_DISPLAY', 5);
+define('DEFAULT_PLAYER', 'Default');
+define('GAME_TIME', 90); // < 3600
+
